18dd7fc8f1bc175b57c454fc1c48e273813f4947
daylish <trubyonrails@gmail.com>
2020-03-18 14:16:50 +0100

    added display microservice

---
+++ display-microservice/src/test/java/etf/nwt/displaymicroservice/DisplayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package etf.nwt.displaymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
