0e4034cf5755b2e21bf4d87d42c416baffdb7bb0
CRATONBiH <mujo_geler@hotmail.com>
2020-03-29 21:47:57 +0200

    Dodani errori za nedefaultne slucajeve

--- list-microservice/src/main/java/etf/nwt/listmicroservice/controller/controller.java
+++ list-microservice/src/main/java/etf/nwt/listmicroservice/controller/controller.java
@@ -1,8 +1,11 @@
 package etf.nwt.listmicroservice.controller;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,37 +25,81 @@ public class Controller {
     private ListaServis listaServis;
 
     @GetMapping("/lists")
-    public List<Lista> getAllLists() {
-        return listaServis.getAllLists();
+    public ResponseEntity<?> getAllLists() {
+        return ResponseEntity.status(HttpStatus.OK).body(listaServis.getAllLists());
     }
 
     @GetMapping("/lists/{userId}")
-    public List<Lista> getLists(@PathVariable("userId") Long userId) {
-        return listaServis.getLists(userId);
+    public ResponseEntity<?> getLists(@PathVariable("userId") Long userId) {
+        
+        if(userId < 1) {
+            Map<String, String> error = new HashMap<String, String>();
+            error.put("message", "userId mora biti cjelobrojni broj veci od 1");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(error);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(listaServis.getLists(userId));
     }
 
     @PostMapping("/lists/new")
-    public void createList(@RequestBody Lista lista) {
+    public ResponseEntity<?> createList(@RequestBody Lista lista) {
         listaServis.createList(lista);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
     
     @GetMapping("/listItems/{listId}")
-    public List<ListItem> getListsItems(@PathVariable("listId") Long listId) {
-        return listaServis.getListsItems(listId);
+    public ResponseEntity<?> getListsItems(@PathVariable("listId") Long listId) {
+
+        if(listId < 1) {
+            Map<String, String> error = new HashMap<String, String>();
+            error.put("message", "listId mora biti cjelobrojni broj veci od 1");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(error);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(listaServis.getListsItems(listId));
     }
 
     @PostMapping("lists/{listId}/new")
-    public void addListItemToList(@PathVariable("listId") Long listId, @RequestBody ListItem li) {
+    public ResponseEntity<?> addListItemToList(@PathVariable("listId") Long listId, @RequestBody ListItem li) {
+
+        if(listId < 1) {
+            Map<String, String> error = new HashMap<String, String>();
+            error.put("message", "listId mora biti cjelobrojni broj veci od 1");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(error);
+        }
+
         listaServis.addListItemToList(listId, li);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("lists/delete/{listId}")
-    public void deleteList(@PathVariable("listId") Long listId) {
+    public ResponseEntity<?> deleteList(@PathVariable("listId") Long listId) {
+
+        if(listId < 1) {
+            Map<String, String> error = new HashMap<String, String>();
+            error.put("message", "listId mora biti cjelobrojni broj veci od 1");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(error);
+        }
+
         listaServis.deleteList(listId);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("lists/{listId}/delete/{listItemId}")
-    public void deleteListItemFromList(@PathVariable("listId") Long listId, @PathVariable("listItemId") Long listItemId) {
+    public ResponseEntity<?> deleteListItemFromList(@PathVariable("listId") Long listId, @PathVariable("listItemId") Long listItemId) {
+        
+        if(listId < 1 || listItemId < 1) {
+            Map<String, String> error = new HashMap<String, String>();
+            error.put("message", "listId i listItemId moraju biti cjelobrojni brojevi veci od 1");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(error);
+        }
+
         listaServis.deleteListItemFromList(listId, listItemId);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- stream-microservice/src/main/java/etf/nwt/streammicroservice/controller/Controller.java
+++ stream-microservice/src/main/java/etf/nwt/streammicroservice/controller/Controller.java
@@ -1,8 +1,12 @@
 package etf.nwt.streammicroservice.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,37 +25,71 @@ public class Controller {
     private StreamService streamService;
 
     @GetMapping("/streams")
-    public List<Stream> getAllStreams() {
-        return streamService.getAllStreams();
+    public ResponseEntity<List<Stream>> getAllStreams() {
+        return ResponseEntity.status(HttpStatus.OK).body(streamService.getAllStreams());
     }
 
     @GetMapping("/streams/{itemId}")
-    public List<Stream> getStreamsByItemId(@PathVariable("itemId") Long itemId) {
-        return streamService.getStreamsByItemId(itemId);
+    public ResponseEntity<?> getStreamsByItemId(@PathVariable("itemId") Long itemId) {
+        if(itemId < 1) {
+            Map<String, String> error = new HashMap<String, String>();
+            error.put("message", "itemId mora biti cjelobrojni broj veci od 1");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(error);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(streamService.getStreamsByItemId(itemId));
     }
 
     @GetMapping("/platforms")
-    public List<Platform> getAllPlatforms() {
-        return streamService.getAllPlatforms();
+    public ResponseEntity<?> getAllPlatforms() {
+        return ResponseEntity.status(HttpStatus.OK).body(streamService.getAllPlatforms());
     }
 
     @PostMapping("/platforms/new")
-    public void addPlatform(@RequestBody Platform platform) {
+    public ResponseEntity<?> addPlatform(@RequestBody Platform platform) {
         streamService.addPlatform(platform);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping("/platforms/{platformId}/new")
-    public void addStream(@PathVariable("platformId") Long platformId, @RequestBody Stream stream) {
+    public ResponseEntity<?> addStream(@PathVariable("platformId") Long platformId, @RequestBody Stream stream) {
+        
+        if(platformId < 1) {
+            Map<String, String> error = new HashMap<String, String>();
+            error.put("message", "platformId mora biti cjelobrojni broj veci od 1");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(error);
+        }
+
         streamService.addStream(platformId, stream);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/platforms/delete/{platformId}")
-    public void deletePlatform(@PathVariable("platformId") Long platformId) {
+    public ResponseEntity<?> deletePlatform(@PathVariable("platformId") Long platformId) {
+        
+        if(platformId < 1) {
+            Map<String, String> error = new HashMap<String, String>();
+            error.put("message", "platformId mora biti cjelobrojni broj veci od 1");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(error);
+        }
+
         streamService.deletePlatform(platformId);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/platforms/{platformId}/delete/{streamId}")
-    public void deleteStream(@PathVariable("platformId") Long platformId, @PathVariable("streamId") Long streamId) {
+    public ResponseEntity<?> deleteStream(@PathVariable("platformId") Long platformId, @PathVariable("streamId") Long streamId) {
+        
+        if(platformId < 1 ||streamId < 1) {
+            Map<String, String> error = new HashMap<String, String>();
+            error.put("message", "platformId i streamId moraju biti cjelobrojni brojevi veci od 1");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                body(error);
+        }
+        
         streamService.deleteStream(platformId, streamId);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
