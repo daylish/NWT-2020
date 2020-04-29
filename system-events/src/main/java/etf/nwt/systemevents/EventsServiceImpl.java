e9a415762c597f6b9303d45014b9fc22635c6de9
adnanel <adnanel94@gmail.com>
2020-04-29 15:29:05 +0200

    properly implement service method

--- system-events/src/main/java/etf/nwt/systemevents/EventsServiceImpl.java
+++ system-events/src/main/java/etf/nwt/systemevents/EventsServiceImpl.java
@@ -5,32 +5,28 @@ import io.grpc.BindableService;
 import io.grpc.ServerServiceDefinition;
 import io.grpc.stub.StreamObserver;
 
-public class EventsServiceImpl implements BindableService {
-	
-	// the method that gets all the relevant information 
-	public void hello(
-			EventRequest request, StreamObserver<EventResponse> responseObserver) {
- 
+public class EventsServiceImpl extends EventsServiceGrpc.EventsServiceImplBase {
+
+    @Override
+    public void hello(
+            etf.nwt.systemevents.EventRequest request, StreamObserver<EventResponse> responseObserver
+    ) {
         String eventResponseText = new StringBuilder()
-          .append("Object type: ")
-          .append(request.getObjectType())
-          .append(", Request type: ")
-          .append(request.getRequestType())
-          .append(", Response type: ")
-          .append(request.getResponseType())
-          .append(", Timestamp: ")
-          .append(request.getEventTimestamp())
-          .toString();
- 
+                .append("Object type: ")
+                .append(request.getObjectType())
+                .append(", Request type: ")
+                .append(request.getRequestType())
+                .append(", Response type: ")
+                .append(request.getResponseType())
+                .append(", Timestamp: ")
+                .append(request.getRequestTimestamp())
+                .toString();
+
         EventResponse response = EventResponse.newBuilder()
-				.setEventResponseText(eventResponseText)
-				.build();
- 
+                .setEventResponseText(eventResponseText)
+                .build();
+
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
-
-	@Override
-	public ServerServiceDefinition bindService() {
-		return ServerServiceDefinition.builder("eventService").build();
-	}}
+}
