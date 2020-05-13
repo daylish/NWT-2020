d7cc363c7086adf9621abe0f86f81e4e32fe3814
adnanel <adnanel94@gmail.com>
2020-05-13 09:24:01 +0200

    autowire eventservice

--- list-microservice/src/main/java/etf/nwt/listmicroservice/config/InterceptorConfiguration.java
+++ list-microservice/src/main/java/etf/nwt/listmicroservice/config/InterceptorConfiguration.java
@@ -1,19 +1,35 @@
 package etf.nwt.listmicroservice.config;
 
 import etf.nwt.listmicroservice.interceptor.EventInterceptor;
+import etf.nwt.systemevents.EventsServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class InterceptorConfiguration implements WebMvcConfigurer {
-
     @Autowired
     EventInterceptor eventInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(eventInterceptor);
+    }
+
+    @Bean
+    EventsServiceGrpc.EventsServiceBlockingStub eventServiceGrpc(
+            @Value("${grpc.host}") String grpcHost,
+            @Value("${grpc.port}") Integer grpcPort
+    ) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(grpcHost, grpcPort)
+                .usePlaintext()
+                .build();
+
+        return EventsServiceGrpc.newBlockingStub(channel);
     }
 }
