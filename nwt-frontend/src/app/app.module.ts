886746e54ae8de432815e5f657117cb965de2ecb
CRATONBiH <mujo_geler@hotmail.com>
2020-05-27 02:36:10 +0200

    Dodana stream komponenta

--- nwt-frontend/src/app/app.module.ts
+++ nwt-frontend/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataDisplayComponent } from './data-display/data-display.component';
 import { UserDisplayComponent } from './user-display/user-display.component';
+import { StreamComponent } from './stream/stream.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataDisplayComponent,
-    UserDisplayComponent
+    UserDisplayComponent,
+    StreamComponent
   ],
   imports: [
     BrowserModule,
