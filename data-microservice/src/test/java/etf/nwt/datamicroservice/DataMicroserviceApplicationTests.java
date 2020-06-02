b2d79cac4d434e93cfd0efece5e61776e4195a60
adnanel <adnanel94@gmail.com>
2020-06-03 01:38:58 +0200

    fix test execution

--- data-microservice/src/test/java/etf/nwt/datamicroservice/DataMicroserviceApplicationTests.java
+++ data-microservice/src/test/java/etf/nwt/datamicroservice/DataMicroserviceApplicationTests.java
@@ -2,13 +2,15 @@ package etf.nwt.datamicroservice;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import etf.nwt.datamicroservice.util.DataMicroserviceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import etf.nwt.datamicroservice.controller.MovieController;
 
-@SpringBootTest
+@SpringBootTest(classes = DataMicroserviceApplication.class)
 class DataMicroserviceApplicationTests {
 	
 	@Autowired
