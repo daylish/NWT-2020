660bac3cdec1552efc5988a16d1fca8fc8cb3165
adnanel <adnanel94@gmail.com>
2020-06-07 19:56:51 +0200

    implement movies api

--- nwt-frontend/src/app/services/data.service.ts
+++ nwt-frontend/src/app/services/data.service.ts
@@ -1,6 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Config} from "./config";
+import {Config} from './config';
+import {Movie} from './model/Movie';
+import {Observable} from 'rxjs';
+import {List} from '../list';
+import {UserService} from './user.service';
+import {ServiceUtils} from './ServiceUtils';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +13,26 @@ import {Config} from "./config";
 export class DataService {
   private url = Config.basepath + '/data-microservice';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userService: UserService) {
+  }
+
+  getAllMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.url + '/movies', ServiceUtils.GetHttpOptions(this.userService));
+  }
+
+  getMovieById(id: number): Observable<Movie> {
+    return this.http.get<Movie>(this.url + '/movies/' + id, ServiceUtils.GetHttpOptions(this.userService));
+  }
+
+  createMovie(movie: Movie): Observable<Movie> {
+    return this.http.post<Movie>(this.url + '/movies/new', movie, ServiceUtils.GetHttpOptions(this.userService));
+  }
+
+  updateMovie(movie: Movie): Observable<Movie> {
+    return this.http.put<Movie>(this.url + '/movies/edit/' + movie.movieID, movie, ServiceUtils.GetHttpOptions(this.userService));
+  }
+
+  deleteMovie(id: number): Observable<any> {
+    return this.http.delete(this.url + '/movies/delete/' + id, ServiceUtils.GetHttpOptions(this.userService));
+  }
 }
